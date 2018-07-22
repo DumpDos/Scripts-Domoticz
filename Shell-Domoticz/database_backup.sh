443beba65886ff4ba4e725249b2bea2ad6c912fe
DumpDos <donvitot@gmail.com>
2018-07-22 19:51:18 +0200

    Create database_backup.sh

---
+++ Shell-Domoticz/database_backup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#-- Variables --#
+dz_ip='192.168.xxx.xxx'
+dz_port='8080'
+
+nas_ip='192.168.xxx.xxx'
+nas_user='user'
+nas_pass='password'
+
+file_emp="/home/Domoticz.db"
+save_emp="/Sauvegarde/Domoticz.db"
+
+##############
+#-- Script --#
+##############
+
+#-- Wget --#
+wget http://$dz_ip:$dz_port/backupdatabase.php -O $file_emp
+
+#-- FTP --#
+ftp -n $nas_ip <<END_SCRIPT
+user $nas_user $nas_pass
+put $file_emp $save_emp
+bye
+END_SCRIPT
+exit 0
